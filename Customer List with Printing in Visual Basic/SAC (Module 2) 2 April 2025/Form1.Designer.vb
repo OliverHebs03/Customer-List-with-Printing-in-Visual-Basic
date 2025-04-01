bc96946efa0cedd881a3e76e399bf32a98c80ab4
OliverHebs03 <Oliverhebs03@gmail.com>
2025-04-02 10:50:15 +1100

    Add files via upload

---
+++ Customer List with Printing in Visual Basic/SAC (Module 2) 2 April 2025/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        lblName = New Label()
+        lblState = New Label()
+        lblABNACN = New Label()
+        lblStructure = New Label()
+        mnuMainStrip = New MenuStrip()
+        FILEToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        PrintPreviewToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        btnClear = New Button()
+        btnAdd = New Button()
+        txtbName = New TextBox()
+        rbtnCompany = New RadioButton()
+        rbtnPartnership = New RadioButton()
+        rbtnSoleTrader = New RadioButton()
+        txtbABNACN = New TextBox()
+        cbState = New ComboBox()
+        grpSearch = New GroupBox()
+        btnStructure = New Button()
+        btnABNACN = New Button()
+        btnState = New Button()
+        btnName = New Button()
+        ofdFilename = New OpenFileDialog()
+        sfdFilename = New SaveFileDialog()
+        PrintPreviewDialog1 = New PrintPreviewDialog()
+        PrintDocument1 = New Printing.PrintDocument()
+        mnuMainStrip.SuspendLayout()
+        grpSearch.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' lblName
+        ' 
+        lblName.AutoSize = True
+        lblName.Location = New Point(34, 61)
+        lblName.Name = "lblName"
+        lblName.Size = New Size(39, 15)
+        lblName.TabIndex = 0
+        lblName.Text = "Name"
+        ' 
+        ' lblState
+        ' 
+        lblState.AutoSize = True
+        lblState.Location = New Point(34, 107)
+        lblState.Name = "lblState"
+        lblState.Size = New Size(33, 15)
+        lblState.TabIndex = 1
+        lblState.Text = "State"
+        ' 
+        ' lblABNACN
+        ' 
+        lblABNACN.AutoSize = True
+        lblABNACN.Location = New Point(34, 153)
+        lblABNACN.Name = "lblABNACN"
+        lblABNACN.Size = New Size(86, 15)
+        lblABNACN.TabIndex = 2
+        lblABNACN.Text = "ABN / ACN No"
+        ' 
+        ' lblStructure
+        ' 
+        lblStructure.AutoSize = True
+        lblStructure.Location = New Point(34, 204)
+        lblStructure.Name = "lblStructure"
+        lblStructure.Size = New Size(55, 15)
+        lblStructure.TabIndex = 3
+        lblStructure.Text = "Structure"
+        ' 
+        ' mnuMainStrip
+        ' 
+        mnuMainStrip.Items.AddRange(New ToolStripItem() {FILEToolStripMenuItem})
+        mnuMainStrip.Location = New Point(0, 0)
+        mnuMainStrip.Name = "mnuMainStrip"
+        mnuMainStrip.Size = New Size(450, 24)
+        mnuMainStrip.TabIndex = 4
+        mnuMainStrip.Text = "MenuStrip1"
+        ' 
+        ' FILEToolStripMenuItem
+        ' 
+        FILEToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, OpenToolStripMenuItem, SaveToolStripMenuItem, PrintPreviewToolStripMenuItem, ExitToolStripMenuItem})
+        FILEToolStripMenuItem.Name = "FILEToolStripMenuItem"
+        FILEToolStripMenuItem.Size = New Size(40, 20)
+        FILEToolStripMenuItem.Text = "FILE"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(143, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(143, 22)
+        OpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.Size = New Size(143, 22)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' PrintPreviewToolStripMenuItem
+        ' 
+        PrintPreviewToolStripMenuItem.Name = "PrintPreviewToolStripMenuItem"
+        PrintPreviewToolStripMenuItem.Size = New Size(143, 22)
+        PrintPreviewToolStripMenuItem.Text = "Print Preview"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(143, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnClear.Location = New Point(69, 273)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(87, 31)
+        btnClear.TabIndex = 5
+        btnClear.Text = "CLEAR"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(231, 273)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(95, 31)
+        btnAdd.TabIndex = 6
+        btnAdd.Text = "ADD"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' txtbName
+        ' 
+        txtbName.Location = New Point(136, 57)
+        txtbName.Name = "txtbName"
+        txtbName.Size = New Size(170, 23)
+        txtbName.TabIndex = 7
+        ' 
+        ' rbtnCompany
+        ' 
+        rbtnCompany.AutoSize = True
+        rbtnCompany.Location = New Point(139, 204)
+        rbtnCompany.Name = "rbtnCompany"
+        rbtnCompany.Size = New Size(77, 19)
+        rbtnCompany.TabIndex = 8
+        rbtnCompany.TabStop = True
+        rbtnCompany.Text = "Company"
+        rbtnCompany.UseVisualStyleBackColor = True
+        ' 
+        ' rbtnPartnership
+        ' 
+        rbtnPartnership.AutoSize = True
+        rbtnPartnership.Location = New Point(241, 204)
+        rbtnPartnership.Name = "rbtnPartnership"
+        rbtnPartnership.Size = New Size(85, 19)
+        rbtnPartnership.TabIndex = 9
+        rbtnPartnership.TabStop = True
+        rbtnPartnership.Text = "Partnership"
+        rbtnPartnership.UseVisualStyleBackColor = True
+        ' 
+        ' rbtnSoleTrader
+        ' 
+        rbtnSoleTrader.AutoSize = True
+        rbtnSoleTrader.Location = New Point(344, 204)
+        rbtnSoleTrader.Name = "rbtnSoleTrader"
+        rbtnSoleTrader.Size = New Size(83, 19)
+        rbtnSoleTrader.TabIndex = 10
+        rbtnSoleTrader.TabStop = True
+        rbtnSoleTrader.Text = "Sole Trader"
+        rbtnSoleTrader.UseVisualStyleBackColor = True
+        ' 
+        ' txtbABNACN
+        ' 
+        txtbABNACN.Location = New Point(139, 150)
+        txtbABNACN.Name = "txtbABNACN"
+        txtbABNACN.Size = New Size(167, 23)
+        txtbABNACN.TabIndex = 11
+        ' 
+        ' cbState
+        ' 
+        cbState.FormattingEnabled = True
+        cbState.Items.AddRange(New Object() {"VIC", "NSW", "QLD", "TAS", "WA", "SA"})
+        cbState.Location = New Point(139, 104)
+        cbState.Name = "cbState"
+        cbState.Size = New Size(121, 23)
+        cbState.TabIndex = 12
+        ' 
+        ' grpSearch
+        ' 
+        grpSearch.Controls.Add(btnStructure)
+        grpSearch.Controls.Add(btnABNACN)
+        grpSearch.Controls.Add(btnState)
+        grpSearch.Controls.Add(btnName)
+        grpSearch.Location = New Point(0, 339)
+        grpSearch.Name = "grpSearch"
+        grpSearch.Size = New Size(451, 194)
+        grpSearch.TabIndex = 13
+        grpSearch.TabStop = False
+        grpSearch.Text = "SEARCH"
+        ' 
+        ' btnStructure
+        ' 
+        btnStructure.Font = New Font("Segoe UI", 11.25F)
+        btnStructure.Location = New Point(231, 114)
+        btnStructure.Name = "btnStructure"
+        btnStructure.Size = New Size(111, 35)
+        btnStructure.TabIndex = 3
+        btnStructure.Text = "Structure"
+        btnStructure.UseVisualStyleBackColor = True
+        ' 
+        ' btnABNACN
+        ' 
+        btnABNACN.Font = New Font("Segoe UI", 11.25F)
+        btnABNACN.Location = New Point(69, 114)
+        btnABNACN.Name = "btnABNACN"
+        btnABNACN.Size = New Size(120, 35)
+        btnABNACN.TabIndex = 2
+        btnABNACN.Text = "ACN / ABN No"
+        btnABNACN.UseVisualStyleBackColor = True
+        ' 
+        ' btnState
+        ' 
+        btnState.Font = New Font("Segoe UI", 11.25F)
+        btnState.Location = New Point(231, 51)
+        btnState.Name = "btnState"
+        btnState.Size = New Size(111, 33)
+        btnState.TabIndex = 1
+        btnState.Text = "State"
+        btnState.UseVisualStyleBackColor = True
+        ' 
+        ' btnName
+        ' 
+        btnName.Font = New Font("Segoe UI", 11.25F)
+        btnName.Location = New Point(69, 51)
+        btnName.Name = "btnName"
+        btnName.Size = New Size(120, 33)
+        btnName.TabIndex = 0
+        btnName.Text = "Name"
+        btnName.UseVisualStyleBackColor = True
+        ' 
+        ' ofdFilename
+        ' 
+        ofdFilename.FileName = "OpenFileDialog1"
+        ' 
+        ' PrintPreviewDialog1
+        ' 
+        PrintPreviewDialog1.AutoScrollMargin = New Size(0, 0)
+        PrintPreviewDialog1.AutoScrollMinSize = New Size(0, 0)
+        PrintPreviewDialog1.ClientSize = New Size(400, 300)
+        PrintPreviewDialog1.Enabled = True
+        PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), Icon)
+        PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        PrintPreviewDialog1.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(450, 529)
+        Controls.Add(grpSearch)
+        Controls.Add(cbState)
+        Controls.Add(txtbABNACN)
+        Controls.Add(rbtnSoleTrader)
+        Controls.Add(rbtnPartnership)
+        Controls.Add(rbtnCompany)
+        Controls.Add(txtbName)
+        Controls.Add(btnAdd)
+        Controls.Add(btnClear)
+        Controls.Add(lblStructure)
+        Controls.Add(lblABNACN)
+        Controls.Add(lblState)
+        Controls.Add(lblName)
+        Controls.Add(mnuMainStrip)
+        MainMenuStrip = mnuMainStrip
+        Name = "Form1"
+        Text = "SupplierList"
+        mnuMainStrip.ResumeLayout(False)
+        mnuMainStrip.PerformLayout()
+        grpSearch.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblState As Label
+    Friend WithEvents lblABNACN As Label
+    Friend WithEvents lblStructure As Label
+    Friend WithEvents mnuMainStrip As MenuStrip
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtbName As TextBox
+    Friend WithEvents rbtnCompany As RadioButton
+    Friend WithEvents rbtnPartnership As RadioButton
+    Friend WithEvents rbtnSoleTrader As RadioButton
+    Friend WithEvents txtbABNACN As TextBox
+    Friend WithEvents cbState As ComboBox
+    Friend WithEvents grpSearch As GroupBox
+    Friend WithEvents btnStructure As Button
+    Friend WithEvents btnABNACN As Button
+    Friend WithEvents btnState As Button
+    Friend WithEvents btnName As Button
+    Friend WithEvents FILEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PrintPreviewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ofdFilename As OpenFileDialog
+    Friend WithEvents sfdFilename As SaveFileDialog
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+
+End Class
---
+++ Customer List with Printing in Visual Basic/SAC (Module 2) 2 April 2025/Form1.vb
@@ -0,0 +1,257 @@
+﻿Imports System.Text
+Imports System.Windows.Forms.MonthCalendar
+
+'Version 0.1
+'Name: Supplier List
+'Author: Oliver Heberle
+'Purpose: For a Software sac, Case study purpose is for the program to act as a way to store suppliers in a list
+
+Public Class Form1
+
+    ' Array to hold customer records in comma-separated format
+    Dim arrSupplierList(1) As String
+
+    ' Index to track the number of entries added to the array
+    Dim intIndex As Integer = 0
+
+    ' Stores the filename for saving or opening files
+    Dim strFilename As String = ""
+
+    '==========================[ FORM RESET FUNCTION ]==========================
+    Private Sub ClearForm()
+        ' Clear all text boxes
+        txtbName.Clear()
+        txtbABNACN.Clear()
+
+        ' Uncheck all quality radio buttons
+        rbtnCompany.Checked = False
+        rbtnPartnership.Checked = False
+        rbtnSoleTrader.Checked = False
+
+        ' Reset dropdown to no selection
+        cbState.SelectedIndex = -1
+    End Sub
+
+    '==========================[ CLEAR BUTTON ]==========================
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearForm() 'Calls clear form funtion when button pressed
+    End Sub
+
+    '==========================[ ADD CUSTOMER BUTTON ]==========================
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Try
+            ' Combine form inputs into a single comma-separated string
+            arrSupplierList(intIndex) = txtbName.Text & "|" & cbState.Text & "|" & txtbABNACN.Text & "|" & GetStructure()
+            intIndex += 1 ' Move to next index in array
+        Catch ex As IndexOutOfRangeException
+            ' Resize array if limit exceeded (dynamic expansion)
+            ReDim Preserve arrSupplierList(arrSupplierList.Length)
+            ' Add data again after resizing
+            arrSupplierList(intIndex) = txtbName.Text & "|" & cbState.Text & "|" & txtbABNACN.Text & "|" & GetStructure()
+            intIndex += 1
+        End Try
+
+        ' Reset the form and return focus to first field
+        ClearForm()
+        txtbName.Focus()
+    End Sub
+
+    '==========================[ STRUCTURE HELPER FUNCTION ]==========================
+    ' Returns selected structure value based on which radio button is checked
+    Private Function GetStructure() As String
+        If rbtnCompany.Checked Then Return "Company"
+        If rbtnPartnership.Checked Then Return "Partnership"
+        If rbtnSoleTrader.Checked Then Return "Sole Trader"
+        Return "" ' No selection
+    End Function
+
+    '==========================[ SAVE TO FILE OPTION ]==========================
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        ' Prompt user with Save File dialog
+        sfdFilename.Filter = "Text Files|*.txt|All Files|*.*"
+        If sfdFilename.ShowDialog() = DialogResult.OK Then
+            strFilename = sfdFilename.FileName
+
+            ' Ensure the file has a .txt extension
+            If Not strFilename.ToLower().EndsWith(".txt") Then
+                strFilename &= ".txt"
+            End If
+
+            ' Call function to write data to file
+            SaveToFile(strFilename)
+        End If
+    End Sub
+
+    ' Actually writes the customer data to the specified file
+    Private Sub SaveToFile(path As String)
+        ' Trim any unused array elements
+        ReDim Preserve arrSupplierList(intIndex - 1)
+
+        ' Open file and write each customer record line-by-line
+        Using writer As New IO.StreamWriter(path)
+            writer.WriteLine("Name,State,ABN/ACN,Structure") ' Write text header
+            For Each line In arrSupplierList
+                writer.WriteLine(line)
+            Next
+        End Using
+    End Sub
+
+    '==========================[ OPEN FILE OPTION ]==========================
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        ' Prompt user with Open File dialog
+        ofdFilename.Filter = "Text Files|*.txt|All Files|*.*"
+        If ofdFilename.ShowDialog() = DialogResult.OK Then
+            strFilename = ofdFilename.FileName
+
+            ' Read file line-by-line
+            Using reader As New IO.StreamReader(strFilename)
+                Dim isFirstLine As Boolean = True
+
+                While Not reader.EndOfStream
+                    Dim line As String = reader.ReadLine()
+
+                    ' Skip header line
+                    If isFirstLine Then
+                        isFirstLine = False
+                        Continue While
+                    End If
+
+                    ' Resize array if needed
+                    If intIndex >= arrSupplierList.Length Then
+                        ReDim Preserve arrSupplierList(arrSupplierList.Length)
+                    End If
+
+                    ' Add record to array
+                    arrSupplierList(intIndex) = line
+                    intIndex += 1
+                End While
+            End Using
+
+            ' Show first record on the form
+            DisplayRecord(arrSupplierList(0))
+        End If
+    End Sub
+
+    '==========================[ DISPLAY A RECORD ON FORM ]==========================
+    Private Sub DisplayRecord(record As String)
+        Dim fields = record.Split(","c)
+
+        ' Set values in form fields
+        txtbName.Text = fields(0)
+        txtbABNACN.Text = fields(2)
+
+
+        ' Set appropriate state radio button
+        Select Case fields(3)
+            Case "Company" : rbtnCompany.Checked = True
+            Case "Partnership" : rbtnPartnership.Checked = True
+            Case "Sole Trader" : rbtnSoleTrader.Checked = True
+        End Select
+
+        ' Set state combo box
+        cbState.Text = fields(1)
+    End Sub
+
+    '==========================[ NEW FILE OPTION ]==========================
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        ' Reset all state and clear form
+        ReDim arrSupplierList(1)
+        intIndex = 0
+        strFilename = ""
+        ClearForm()
+    End Sub
+    '==========================[ EXIT APPLICATION ]==========================
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End ' Closes the application
+    End Sub
+    '==========================[ SEARCH FUNCTION ]==========================
+    ' Looks through all records for a match in the specified field
+    Private Sub SearchRecord(keyword As String, fieldIndex As Integer)
+        For Each record In arrSupplierList
+            Dim fields = record.Split(","c)
+            ' Case-insensitive match
+            If fields(fieldIndex).Trim().ToUpper() = keyword.Trim().ToUpper() Then
+                DisplayRecord(record)
+                Return
+            End If
+        Next
+
+        ' If no match found
+        MessageBox.Show("Record not found!", "Search", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ClearForm()
+    End Sub
+
+    '==========================[ SEARCH BY NAME ]==========================
+    Private Sub btnName_Click(sender As Object, e As EventArgs) Handles btnName.Click
+        Dim keyword As String = InputBox("Enter Name to search")
+        SearchRecord(keyword, 0) ' 0 = Name field
+    End Sub
+
+    '==========================[ SEARCH BY STATE ]==========================
+    Private Sub btnState_Click(sender As Object, e As EventArgs) Handles btnState.Click
+        Dim keyword As String = InputBox("Enter a state is search")
+        SearchRecord(keyword.ToUpper(), 1) ' 1 = State field
+    End Sub
+
+    '==========================[ SEARCH BY ABN/ACN ]==========================
+    Private Sub btnABNACN_Click(sender As Object, e As EventArgs) Handles btnABNACN.Click
+        Dim keyword As String = InputBox("Enter an ABN or ACN to search")
+        SearchRecord(keyword, 2) ' 2 = AVN/ACN field
+    End Sub
+    '==========================[ SEARCH BY STRUCTURE ]==========================
+    Private Sub btnStructure_Click(sender As Object, e As EventArgs) Handles btnStructure.Click
+        Dim keyword As String = InputBox("Enter Structure to search")
+        SearchRecord(keyword.ToUpper(), 3) ' 3 = Structure field
+    End Sub
+    '==========================[ PRINT PREVIEW OPTION ]==========================
+    Private Sub PrintPreviewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintPreviewToolStripMenuItem.Click
+        ' Set print orientation and show preview window
+        PrintDocument1.DefaultPageSettings.Landscape = True
+        PrintPreviewDialog1.Document = PrintDocument1
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+
+    '==========================[ PRINTING THE CUSTOMER LIST ]==========================
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        ' Ensure array doesn't contain empty slots
+        ReDim Preserve arrSupplierList(intIndex - 1)
+
+        ' Define fonts for print layout
+        Dim TitleFont As New Font("Arial", 18, FontStyle.Bold)
+        Dim HeaderFont As New Font("Arial", 10, FontStyle.Bold)
+        Dim BodyFont As New Font("Calibri", 10)
+
+        ' Initial vertical position for printing
+        Dim yPosition As Integer = 60
+
+        ' Define x-coordinates for each column
+        Dim xName = 50
+        Dim xState = 200
+        Dim xABNACN = 300
+        Dim xStructure = 420
+
+
+        ' Print title and header row
+        e.Graphics.DrawString("Supplier List", TitleFont, Brushes.DarkGreen, xName, 20)
+        e.Graphics.DrawString("Name", HeaderFont, Brushes.Black, xName, yPosition)
+        e.Graphics.DrawString("State", HeaderFont, Brushes.Black, xState, yPosition)
+        e.Graphics.DrawString("ABN/ACN", HeaderFont, Brushes.Black, xABNACN, yPosition)
+        e.Graphics.DrawString("Structure", HeaderFont, Brushes.Black, xStructure, yPosition)
+
+
+        yPosition += 25 ' Space between headers and records
+
+        ' Print each customer record
+        For i = 0 To intIndex - 1
+            Dim data = arrSupplierList(i).Split(","c)
+            If data.Length = 4 Then ' Ensure complete record
+                e.Graphics.DrawString(data(0), BodyFont, Brushes.Black, xName, yPosition)
+                e.Graphics.DrawString(data(1), BodyFont, Brushes.Black, xState, yPosition)
+                e.Graphics.DrawString(data(2), BodyFont, Brushes.Black, xABNACN, yPosition)
+                e.Graphics.DrawString(data(3), BodyFont, Brushes.Black, xStructure, yPosition)
+                yPosition += 20 ' Move to next line
+            End If
+        Next
+    End Sub
+End Class
+
